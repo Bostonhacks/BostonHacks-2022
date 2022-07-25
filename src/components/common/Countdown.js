48348be096847c86976654fb93d7628877816c56
firminm <matthewjf1@gmail.com>
2022-07-24 21:36:03 -0400

    Added Countdown bar

---
+++ src/components/common/Countdown.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+/**
+ * @author Matt Firmin, Simran Singh
+ */
+
+const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    let deadline = new Date(`11/12/${year}`);   // Will always say the current year >:)
+    deadline.setHours(10, 20, 0);
+    let difference = +deadline - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+        timeLeft = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / 1000 / 60) % 60),
+            seconds: Math.floor((difference / 1000) % 60)
+        };
+    }
+    console.log(timeLeft);
+    return timeLeft;
+}
+
+
+
+export default function Coundown() {
+    const [cTime, setTime] = useState(Date.now());
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(Date.now()), 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    var time = calculateTimeLeft();
+    console.log(time);
+    return (
+        <div id="Countdown">
+            <h2>{time.days} Days {time.hours}:{time.minutes}:{time.seconds}</h2>
+        </div>
+    );
+}
+
