6cab10d04b1122c32c4f24c62093ab65fdb88294
DanYu12 <dan.daniel.yu@gmail.com>
2022-08-12 21:09:14 -0400

    Added gradient svg and title section

--- src/views/Sponsor.js
+++ src/views/Sponsor.js
@@ -1,14 +1,16 @@
-import * as React from 'react'
-import SVGTest from '../components/sponsor/SVGTest'
-import SponsorTable from '../components/sponsor/SponsorTable'
+import * as React from 'react';
+import SponsorTitleSection from '../components/sponsor/svg/SponsorTitleSection';
+import SponsorTable from '../components/sponsor/SponsorTable';
+import BackgroundGradient from '../components/sponsor/svg/BackgroundGradient';
 
 // Sponsorship page
 export default function Sponsor() {
-    return (
-        <div>
-            <h1>Sponsor (Public)</h1>
-            <SponsorTable />
-            <SVGTest />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Sponsor (Public)</h1>
+      <SponsorTable />
+      <SponsorTitleSection />
+      <BackgroundGradient />
+    </div>
+  );
 }
