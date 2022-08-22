048f75b455bf9944cc981201020c655a8760628f
mraigoza <49137050+mraigoza@users.noreply.github.com>
2022-08-21 22:37:35 -0700

    Update styling

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,18 +1,14 @@
 import * as React from "react";
-import {
-    Link,
-} from "react-router-dom";
 import "./Footer.css";
-import NavbarLogo from "./svg/NavbarLogo";
 import SocialMediaIcon from "./svg/SocialMediaIcon";
-import Coundown from "./Countdown";
+import Countdown from "./Countdown";
 
 // Navigation bar
 export default function Footer() {
     return (
         <div>
-            <Coundown/>
-            <ul class="footer">
+            <Countdown/>
+            <ul className="footer">
                 <li>
                     <SocialMediaIcon platform='facebook'/>
                 </li>
@@ -22,11 +18,6 @@ export default function Footer() {
                 <li>
                     <SocialMediaIcon platform='Twitter' />
                 </li>
-                {/* <li>
-                    <a target='' href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf'>MLH Code of Conduct</a>
-                    <span> | </span>
-                    <Link to='/privacy'>Privacy Policy</Link>
-                </li> */}
             </ul>
         </div>
     );
