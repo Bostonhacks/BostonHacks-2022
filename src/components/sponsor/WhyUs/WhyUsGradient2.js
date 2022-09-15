236b9376e7e28868eafad3f4165fe57fcd1ff7d6
DanYu12 <dan.daniel.yu@gmail.com>
2022-09-14 20:19:22 -0400

    added more positioning

---
+++ src/components/sponsor/WhyUs/WhyUsGradient2.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const WhyUsGradient2 = () => {
+  return (
+    <svg
+      className="why-us-gradient-2"
+      width="1063"
+      height="1139"
+      viewBox="0 0 1063 1139"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_f_310_1330)">
+        <ellipse
+          cx="324.016"
+          cy="569.414"
+          rx="190.434"
+          ry="624.34"
+          transform="rotate(57.8756 324.016 569.414)"
+          fill="#AA9DAD"
+          fill-opacity="0.35"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_310_1330"
+          x="-414.444"
+          y="0.231689"
+          width="1476.92"
+          height="1138.37"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="100"
+            result="effect1_foregroundBlur_310_1330"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default WhyUsGradient2;
