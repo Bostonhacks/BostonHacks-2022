974ccef5721c752d0c2f1198b01a50931cd3fd41
DanYu12 <dan.daniel.yu@gmail.com>
2022-10-09 20:14:56 -0400

    Removed Navbar changes

--- src/components/common/NavigationBar.js
+++ src/components/common/NavigationBar.js
@@ -1,52 +1,56 @@
-import React, { useEffect } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import {
-    Link,
-    useNavigate
-} from "react-router-dom";
-import { auth, logout } from "../../firebase/firebase-config";
-import NavbarLogo from "./svg/NavbarLogo";
-import "./NavigationBar.css"
-import styled from "styled-components";
+import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth, logout } from '../../firebase/firebase-config';
+import NavbarLogo from './svg/NavbarLogo';
+import './NavigationBar.css';
+import styled from 'styled-components';
 
 // Navigation bar
 export default function NavigationBar() {
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
 
-    const Button = styled.button`
-      font-family: 'Montserrat';
-      border: 0;
-      cursor: pointer;
-      padding: 14px 5px;
-      height: 100%;
-      box-shadow: none;
-      font-weight: none;
-      font-weight: inherit;
-      font-size: inherit;
-      color: white;
-    `;
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
 
-    useEffect(() => {
-      if (loading) return;
-    }, [loading]);
+  const Button = styled.button`
+    font-family: 'Montserrat';
+    border: 0;
+    cursor: pointer;
+    padding: 14px 5px;
+    height: 100%;
+    box-shadow: none;
+    font-weight: none;
+    font-weight: inherit;
+    font-size: inherit;
+    color: white;
+  `;
 
-    return (
-      <div>
-        <ul className="nav">
-          <li className="logo">
-            <Link to="/"><NavbarLogo /></Link>
-          </li>
-          <li className="button">
-            {user && <Button onClick={handleLogout}>LOGOUT</Button>}
-          </li>
-          <li className="navItem">
-            <Link to="/sponsor">SPONSORS</Link>
-          </li>
-          <li className="navItem">
-            <Link to="/application">APPLY</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (loading) return;
+  }, [loading]);
+
+  return (
+    <div>
+      <ul className="nav">
+        <li className="logo">
+          <Link to="/">
+            <NavbarLogo />
+          </Link>
+        </li>
+        <li className="button">
+          {user && <Button onClick={handleLogout}>LOGOUT</Button>}
+        </li>
+        <li className="navItem">
+          <Link to="/sponsor">SPONSORS</Link>
+        </li>
+        <li className="navItem">
+          <Link to="/application">APPLY</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
