5528f9f23c47eaaed91d6702297e089ab4bfa962
mraigoza <49137050+mraigoza@users.noreply.github.com>
2022-10-11 19:10:42 -0400

    removed changes

--- src/components/common/NavigationBar.js
+++ src/components/common/NavigationBar.js
@@ -1,56 +1,57 @@
-import React, { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import { auth, logout } from '../../firebase/firebase-config';
-import NavbarLogo from './svg/NavbarLogo';
-import './NavigationBar.css';
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+    Link,
+    useNavigate
+} from "react-router-dom";
+import { auth, logout } from "../../firebase/firebase-config";
+import NavbarLogo from "./svg/NavbarLogo";
+import "./NavigationBar.css"
+import styled from "styled-components";
 
 // Navigation bar
 export default function NavigationBar() {
-  const [user, loading] = useAuthState(auth);
-  const navigate = useNavigate();
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate();
+  
+    const handleLogout = () => {
+      logout();
+      navigate("/");
+    };
 
-  const handleLogout = () => {
-    logout();
-    navigate('/');
-  };
+    const Button = styled.button`
+      font-family: 'Montserrat';
+      border: 0;
+      cursor: pointer;
+      padding: 14px 5px;
+      height: 100%;
+      box-shadow: none;
+      font-weight: none;
+      font-weight: inherit;
+      font-size: inherit;
+      color: white;
+    `;
 
-  const Button = styled.button`
-    font-family: 'Montserrat';
-    border: 0;
-    cursor: pointer;
-    padding: 14px 5px;
-    height: 100%;
-    box-shadow: none;
-    font-weight: none;
-    font-weight: inherit;
-    font-size: inherit;
-    color: white;
-  `;
+    useEffect(() => {
+      if (loading) return;
+    }, [loading]);
 
-  useEffect(() => {
-    if (loading) return;
-  }, [loading]);
-
-  return (
-    <div>
-      <ul className="nav">
-        <li className="logo">
-          <Link to="/">
-            <NavbarLogo />
-          </Link>
-        </li>
-        <li className="button">
-          {user && <Button onClick={handleLogout}>LOGOUT</Button>}
-        </li>
-        <li className="navItem">
-          <Link to="/sponsor">SPONSORS</Link>
-        </li>
-        <li className="navItem">
-          <Link to="/application">APPLY</Link>
-        </li>
-      </ul>
-    </div>
-  );
-}
+    return (
+      <div>
+        <ul className="nav">
+          <li className="logo">
+            <Link to="/"><NavbarLogo /></Link>
+          </li>
+          <li className="button">
+            {user && <Button onClick={handleLogout}>LOGOUT</Button>}
+          </li>
+          <li className="navItem">
+            <Link to="/sponsor">SPONSORS</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/application">APPLY</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
