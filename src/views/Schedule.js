2bd046b81b208ebc63963db3ce201775b15f031b
Dave Godfrey <davegod@bu.edu>
2022-10-16 20:48:35 -0400

    added schedule times

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -1,11 +1,137 @@
 import './Schedule.css';
-import ScheduleFull from '../components/schedule/schedule'
 
 export default function Schedule() {
     return (
-        <div className='schedule'>
-            <ScheduleFull />
-        </div>
-
+        <table>
+            <thead>
+            <tr>
+                <th></th>
+                <th>Saturday 11/12</th>
+                <th>Sunday 11/13</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <time>1:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>2:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>3:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>4:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>5:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>6:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>7:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>8:00 AM</time>
+                <td>Monte Falco</td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>9:00 AM</time>
+                <td>Check-in for hackers, sponsors, volouteers, etc.. (Stone Lobby) </td>
+                <td>1658</td>
+            </tr>
+            <tr>
+                <time>10:00 AM</time>
+                <td>Begin hacking/finding teams (10:30)<br></br>Test Data (10:35)</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>11:00 AM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>12:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>1:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>2:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>3:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>4:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>5:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>6:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>7:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>8:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>9:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>10:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>11:00 PM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            <tr>
+                <time>12:00 AM</time>
+                <td>Monte Falterona</td>
+                <td>1654</td>
+            </tr>
+            </tbody>
+        </table>
     );
 }
