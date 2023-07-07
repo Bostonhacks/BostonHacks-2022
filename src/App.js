4d03607f3e64535b33869bdc6c1deb911be3ef80
DanYu12 <dan.daniel.yu@gmail.com>
2023-07-06 23:48:18 -0400

    added sponsor us

--- src/App.js
+++ src/App.js
@@ -17,23 +17,36 @@ const App = () => {
         <h1 className="bhacks-text">Bostonhacks 2024</h1>
       </div>
       <div className="coming-soon-container">
-        <h1 className="coming-soon-text">Coming Soon.</h1>
+        <h1 className="coming-soon-text">Coming Soon Nov. 18-19</h1>
       </div>
-      <div className="buttons-container">
-        <a
-          href="https://organize.mlh.io/participants/events/9891-bostonhacks"
-          className="apply-button"
-        >
-          Apply Here
-        </a>
-        <a
-          href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-          className="mlh-code-of-conduct"
-        >
-          MLH Code of Conduct
-        </a>
+      <div className="outer-buttons-container">
+        <div className="buttons-container">
+          <a
+            href="https://organize.mlh.io/participants/events/9891-bostonhacks"
+            className="apply-button"
+          >
+            Apply Here
+          </a>
+          <a
+            href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+            className="mlh-code-of-conduct"
+          >
+            MLH Code of Conduct
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1QXGi1T9DDUsQ0gbiHKvN5lo3pR0fJw2f/view?usp=sharing"
+            className="sponsor-us-button"
+          >
+            Sponsor Us!
+          </a>
+        </div>
       </div>
-      <div className="mlh-code-of-conduct-container"></div>
+      <form className="email-form">
+        <label className="email-label">
+          Interested in hearing more? Join our email list!
+        </label>
+        <input className="email-input" type="text" placeholder="email" />
+      </form>
     </div>
   );
 };
